1d8ab3575647d29cad99df9f6bba19cdfc211057
Rohan <kamathrohan.9903@gmail.com>
2025-05-21 16:09:01 +0530

    added problem leetcode 3341

---
+++ graph/problem_3341.cpp
@@ -0,0 +1,51 @@
+// Problem Link: https://leetcode.com/problems/find-minimum-time-to-reach-last-room-i/
+
+// Approach: Djikstra Algorithm
+// Time Complexity: O(m*n*log(m*n))
+// Space Complexity: O(m*n)
+
+class Solution {
+public:
+  typedef pair<int,pair<int,int>> P;
+  vector<vector<int>>directions = {{-1,0}, {0,1}, {0,-1}, {1,0}};
+
+  int minTimeToReach(vector<vector<int>>& moveTime) {
+    int m = moveTime.size();
+    int n = moveTime[0].size();
+
+    vector<vector<int>>result(m,vector<int>(n, INT_MAX));
+    priority_queue<P,vector<P>,greater<P>>pq;
+
+    result[0][0] = 0;
+    pq.push({0, {0,0}});
+
+    while(!pq.empty()){
+      int curTime = pq.top().first;
+      pair<int,int> coord = pq.top().second;
+      int i = coord.first;
+      int j = coord.second;
+      pq.pop();
+
+      if(i == m-1 && j == n-1) {
+        return curTime;
+      }
+
+      for(auto &dir : directions) {
+        int i_ = i + dir[0];
+        int j_ = j + dir[1];
+
+        if (i_ >= 0 && i_ < m && j_ >= 0 && j_ < n) {
+          int waitTime = max(moveTime[i_][j_] - curTime, 0);
+          int arrTime = waitTime + curTime + 1;
+
+          if(result[i_][j_] > arrTime) {
+            result[i_][j_] = arrTime;
+            pq.push({arrTime, {i_,j_}});
+          }
+        }
+      } 
+    }
+
+    return -1;
+  }
+};
